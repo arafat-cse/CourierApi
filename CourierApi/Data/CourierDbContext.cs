919838dc476b47d42c669e90fb7006c44e03f6be
arafat-cse <arafat.tufan15@gmail.com>
2024-12-03 22:45:37 +0600

    Add project files.

---
+++ CourierApi/Data/CourierDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CourierApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourierApi.Data
+{
+    public class CourierDbContext : DbContext
+    {
+        public CourierDbContext(DbContextOptions<CourierDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Company> Companys { get; set; }
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<BranchStaff> BranchesStaffs { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<DeliveryCharge> DeliveryCharges { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Parcel> Parsers { get; set; }
+        public DbSet<ParcelType> ParsersTypes { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Van> vans { get; set; }
+    }
+}
---
+++ CourierApi/Models/Branch.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourierApi.Models
+{
+    public class Branch
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int branchId { get; set; }
+        public string? branchName { get; set; }
+        public string? address { get; set; }
+        public int ParentId { get; set; }
+        public string? createBy { get; set; }
+        public DateTime? createDate { get; set; }
+        public string? updateBy { get; set; }
+        public DateTime? updateDate { get; set; }
+        public bool IsActive { get; set; }
+        public virtual ICollection<Parcel>? Parcels { get; set; }
+    }
+}
