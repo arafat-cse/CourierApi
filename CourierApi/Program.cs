88ee3158443fbaaf8b461bab81b9e3027209188c
arafat-cse <arafat.tufan15@gmail.com>
2024-12-04 13:33:49 +0600

    Update Controller

--- CourierApi/Program.cs
+++ CourierApi/Program.cs
@@ -5,6 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<CourierDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("AppCon")));
+//Add Code
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
