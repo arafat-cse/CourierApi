76046d5f41d373204e18d7997258212c134faf8c
habiburrahman1836 <habibrahmancmt@gmail.com>
2024-12-14 11:18:29 +0600

    Designation table create

--- CourierApi/Models/Designation.cs
+++ CourierApi/Models/Designation.cs
@@ -1,9 +1,20 @@
-﻿namespace CourierApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourierApi.Models
 {
     public class Designation
     {
-        public int designationId {  get; set; }
-        public string designationName { get; set; }
-        public virtual ICollection<Staff>? Staffs { get; set; }
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int designationId { get; set; }
+        public string? DesignationTitle { get; set; }
+        public string? Description { get; set; }
+        public string? SalaryRange { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string createBy { get; set; }
+        public DateTime createDate { get; set; }
+        public string? updateBy { get; set; }
+        public DateTime? updateDate { get; set; }
+        public ICollection<Staff> StaffMembers { get; set; }
     }
 }
