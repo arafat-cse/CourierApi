d89d0e3d944b67776210fa5acc286d6ab95f48e1
habiburrahman1836 <habibrahmancmt@gmail.com>
2024-12-15 14:03:13 +0600

    percetypeupdate

--- CourierApi/Controllers/BranchesController.cs
+++ CourierApi/Controllers/BranchesController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CourierApi.Data;
 using CourierApi.Models;
-
 namespace CourierApi.Controllers
 {
     [Route("api/[controller]")]
@@ -139,7 +138,6 @@ namespace CourierApi.Controllers
                     throw;
                 }
             }
-
             return NoContent();
         }
 
